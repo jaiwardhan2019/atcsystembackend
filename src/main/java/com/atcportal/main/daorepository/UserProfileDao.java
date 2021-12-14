325149273105d32ef41c0b88ebd903318e6582c5
Dinesh Rajput <admin@dineshonjava.com>
2021-12-14 22:12:55 +0530

    code changes as per email

--- src/main/java/com/atcportal/main/daorepository/UserProfileDao.java
+++ src/main/java/com/atcportal/main/daorepository/UserProfileDao.java
@@ -2,6 +2,8 @@ package com.atcportal.main.daorepository;
 
 import com.atcportal.main.models.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 /*
 * https://howtodoinjava.com/hibernate/hibernate-one-to-many-mapping/
 * https://www.baeldung.com/jpa-queries-custom-result-with-aggregation-functions
@@ -10,5 +12,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserProfileDao extends JpaRepository<UserProfile,  Integer> {
 
+    List<UserProfile> findByUserId(int userId);
 
+    void deleteByUserIdAndProfileId(int userId, String profileId);
 }
