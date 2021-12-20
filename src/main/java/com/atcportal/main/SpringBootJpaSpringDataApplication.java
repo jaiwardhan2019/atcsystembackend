f7b324e40eece2b9a79ad4db1fa63217d82462a4
Dinesh Rajput <admin@dineshonjava.com>
2021-12-20 15:29:53 +0530

    code

--- src/main/java/com/atcportal/main/SpringBootJpaSpringDataApplication.java
+++ src/main/java/com/atcportal/main/SpringBootJpaSpringDataApplication.java
@@ -2,8 +2,10 @@ package com.atcportal.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.atcportal")
 public class SpringBootJpaSpringDataApplication {
 
 	public static void main(String[] args) {
