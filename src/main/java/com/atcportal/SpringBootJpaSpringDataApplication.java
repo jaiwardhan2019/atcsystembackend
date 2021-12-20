91f1136cecb1c4521c148d10ab95e79aee975d20
Dinesh on Java <admin@dineshonjava.com>
2021-12-20 15:48:27 +0530

    Dineshonjava (#5)
    * tests adding
    
    * code
    
    * code changes
    
    * code changes

--- src/main/java/com/atcportal/SpringBootJpaSpringDataApplication.java
+++ src/main/java/com/atcportal/SpringBootJpaSpringDataApplication.java
@@ -1,7 +1,8 @@
-package com.atcportal.main;
+package com.atcportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SpringBootJpaSpringDataApplication {
